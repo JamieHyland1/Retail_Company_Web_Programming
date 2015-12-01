784a2196ee15666821de7661ccabfd2e4d88d17e
JamieHyland1 <Jamiehyland01@gmail.com>
2015-12-01 21:54:09 +0000

    Second Commit, added JavaScript File, added validation, finished validation.

--- Validate.js
+++ Validate.js
@@ -1,5 +1,41 @@
 window.onload = function ()
 {
+    
+    function isNumber(n)
+    {
+        return !isNaN(parseInt(n)) && isFinite(n);
+    }
+    function isValidDateFormat(date)
+    {
+        var re = /^d{2}\/\d{2}\/\d{4}$/;
+        return re.test(date);
+    }
+    function isDate(date)
+    {
+        var parts = date.split("/");
+        var days = parseInt(parts[0], 10);
+        var month = parseInt(parts[1], 10);
+        var year = parseInt(parts[2], 10);
+        
+        var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        
+        //adjusting for leap years
+        if(year%400 == 0 ||(year%100 != 0 && year%4 === 0))
+        {
+            monthLength[1] = 29;
+        }
+        
+        return (year >= 1000 && year <= 3000 &&
+                month >= 1 && month <= 12 &&
+                days >= 1 && days <=  monthLength[month - 1]);
+    }
+    function isValidEmailFormat(mail)
+    {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(mail);
+    }
+
+
     var submitBtn = document.getElementsByID('submit');
     
     submitBtn.addEventListener('click', function(event)
@@ -55,6 +91,44 @@ window.onload = function ()
          if(username == "")
          {
              userNameErrorElement.innerHTML = "UserName is required";
+             valid = false;
+         }
+         if(password == "")
+         {
+             passwordErrorElement.innerHTML = "Password is required";
+             valid = false;
+         }
+         if(mail == "" && !isvalidEmailFormat(mail))
+         {
+             mailErrorElement.innerHTML = "E-mail is required";
+             valid = false;
+         }
+         if(phone == "" && !isNumber(phone))
+         {
+             phoneErrorElement.innerHTML = "Phone number must be a number";
+             valid = false;
+         }
+         else if(phone !== "" && isNumber(phone))
+         {
+             var phoneNum = parseInt(phone);
+             if(phoneNum.toString().length < 0 || phoneNum.toString().length > 10)
+             {
+                 phoneErrorElement.innerHTML = "Phone number must be 0-10 characters long";
+                 valid = false;
+             }
+         }
+         if(date == "" && isvalidDateFormat(date))
+         {
+             dateErrorElement.innerHTML = "A date is required in the form dd/mm/yyyy";
+             valid = false;
+         }
+         else if (date != "" && !isvalidDateFormat(date))
+         {
+             dateErrorElement.innerHTML = "A date is required";
+         }
+         if(location == "")
+         {
+             locationErrorElement.innerHTML = "A store location is required";
              valid = false;
          }
          //=====================================================================
